e5b9ad0b5d6d9d6fa3c2f8bd14c10bc1fe75cd1a
subhendudataengineer <subhendu.dataengineer@gmail.com>
2022-12-22 21:04:24 +0100

    Cluster Policy Definition and Acl Related Change

---
+++ utilities/populate_template.py
@@ -0,0 +1,6 @@
+""" Generic template generator
+This script generate json file based on a cluster definition JSON
+and a template JSON. The pre-requisite is to have a cluster-definition.json
+and a *-template.json file or files in a folder
+"""
+
